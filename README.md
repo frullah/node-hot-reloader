f7cef2ff29ebb87e669bfb6d59f21e66f15eee62
frullah <frullah12@gmail.com>
2018-12-24 02:29:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Nodejs hot reloader
+
+### Installation
+```
+npm i git://github.com:frullah/node-hot-reloader.git -D
+
+or with ssh
+
+npm i git+ssh://github.com:frullah/node-hot-reloader.git -D
+```
+
+### Usage
+```
+hot-reloader <entry-file> [...options]
+
+Options:
+  --version    Show version number                                     [boolean]
+  -h, --help   Show help                                               [boolean]
+  -w, --watch  Watch path target                                        [string]
+```
---
+++ bin/hot-reloader.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const yargs = require('yargs');
+const hotReload = require('../lib');
+const packageJson = require('../package.json');
+
+// const usage = `Hot reload for nodejs
+// Usage: $0 <entry-file> [...options]`;
+
+const cli = yargs
+  .options({
+    watch: {
+      alias: 'w',
+      description: 'Watch path target',
+      string: true,
+      array: true,
+      normalize: true,
+    },
+    verbose: {
+      alias: 'V',
+      description: 'Verbose all processed',
+      boolean: true,
+      default: true,
+    },
+  })
+  .version(packageJson.version)
+  .help();
+
+const {argv} = cli;
+
+console.log(argv);
+
+if (argv._.length == 0) {
+  console.log('Need entry file');
+  process.exit(0);
+}
+
+hotReload.watch({
+  targets: argv.watch,
+  entryFile: argv._[0],
+});
